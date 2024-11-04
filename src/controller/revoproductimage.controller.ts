accc7103ab05bc340dfb9d3a4d98a257ae3ebb13
Harish Raja G <harish@clouddesk.ae>
2024-11-04 11:28:54 +0530

    changes

--- src/controller/revoproductimage.controller.ts
+++ src/controller/revoproductimage.controller.ts
@@ -1,15 +1,16 @@
-
 import { revoimageservice } from "../service/revoproductimage.service.js";
 
-export module revoimagecontroller {
-    export const uploadimage = async (request: any,reply : any) => {
-        try {
-         let uploadimageresult = await revoimageservice.uploadimage(request,reply);
-         reply.send(uploadimageresult);
-        } catch (error) {
-          console.log(error.message);
-          reply.send(error.message);
-        }
+export const revoimagecontroller = {
+  uploadimage: async (request: any, reply: any) => {
+    try {
+      let uploadimageresult = await revoimageservice.uploadimage(
+        request,
+        reply
+      );
+      reply.send(uploadimageresult);
+    } catch (error) {
+      console.log(error.message);
+      reply.send(error.message);
     }
-
-}
+  },
+};
--- src/service/revoproductimage.service.ts
+++ src/service/revoproductimage.service.ts
@@ -2,21 +2,18 @@ import axios from "axios";
 import imageResize from "../utils/imageresize.js";
 import { REVO_PRODUCT_IMAGE_API } from "../utils/config.js";
 
-export module revoimageservice {
-    export const uploadimage = async (request: any, reply: any) => {
-        try {
-            console.log(request.params.productid, 'req.body');
-            let data: any = await imageResize(request);
-            data.productid = request.params.productid
-            let dataresult = await axios.post(REVO_PRODUCT_IMAGE_API, data);
-            console.log(dataresult, 'dataresult');
-            // reply.send(dataresult.data.product);
-            return dataresult.data.product;
-            // reply.send("test");
-            // console.log(dataresult);
-        } catch (error) {
-            console.log(error.message);
-            reply.send(error.message);
-        }
+export const revoimageservice = {
+  uploadimage: async (request: any, reply: any) => {
+    try {
+      console.log(request.params.productid, "req.body");
+      let data: any = await imageResize(request);
+      data.productid = request.params.productid;
+      let dataresult = await axios.post(REVO_PRODUCT_IMAGE_API, data);
+      console.log(dataresult, "dataresult");
+      return dataresult.data.product;
+    } catch (error) {
+      console.log(error.message);
+      return error.message;
     }
-}
+  },
+};
